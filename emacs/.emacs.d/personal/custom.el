c1cbefd9227431cfcfb46e24dbe92647555e5351
Mark Ferlatte <ferlatte@cryptio.net>
2015-09-04 17:43:18 -0700

    Add prelude stuff.

---
+++ emacs/.emacs.d/personal/custom.el
@@ -0,0 +1,64 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 307 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(default-frame-alist
+    (quote
+     ((menu-bar-lines . 1)
+      (tool-bar-lines . 0)
+      (foreground-color . "Black")
+      (background-color . "White")
+      (cursor-type . box)
+      (cursor-color . "Red")
+      (vertical-scroll-bars . right)
+      (internal-border-width . 0)
+      (left-fringe . 1)
+      (right-fringe)
+      (fringe))))
+ '(fci-rule-color "#383838")
+ '(global-hl-line-mode t)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode nil t)
+ '(size-indication-mode t)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(visual-line-mode nil t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/.emacs.d/personal/init.el
@@ -0,0 +1,2 @@
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+
