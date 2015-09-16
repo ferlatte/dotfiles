7af9804b468370776964cc6b9dc4d2f3c97162eb
Mark Ferlatte <ferlatte@cryptio.net>
2015-09-15 22:15:58 -0700

    Solarized.

--- emacs/.emacs.d/personal/custom.el
+++ emacs/.emacs.d/personal/custom.el
@@ -5,34 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(aquamacs-additional-fontsets nil t)
- '(aquamacs-customization-version-id 307 t)
- '(aquamacs-tool-bar-user-customization nil t)
- '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-enabled-themes (quote (solarized-light nil)))
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(default-frame-alist
-    (quote
-     ((menu-bar-lines . 1)
-      (tool-bar-lines . 0)
-      (foreground-color . "Black")
-      (background-color . "White")
-      (cursor-type . box)
-      (cursor-color . "Red")
-      (vertical-scroll-bars . right)
-      (internal-border-width . 0)
-      (left-fringe . 1)
-      (right-fringe)
-      (fringe))))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(fci-rule-color "#383838")
- '(global-hl-line-mode t)
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
- '(ns-tool-bar-display-mode (quote both) t)
- '(ns-tool-bar-size-mode nil t)
- '(size-indication-mode t)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
    (quote
@@ -54,8 +34,7 @@
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
      (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3")
- '(visual-line-mode nil t))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
