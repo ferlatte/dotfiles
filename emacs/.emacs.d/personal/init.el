d23a5f50ae04202fe036172be02cf8c7a91e926a
Mark Ferlatte <ferlatte@cryptio.net>
2017-02-23 20:45:51 -0800

    Fix flycheck warnings.

--- emacs/.emacs.d/personal/init.el
+++ emacs/.emacs.d/personal/init.el
@@ -1,6 +1,17 @@
+;;; init.el --- my personal init.
+;;; Commentary:
+;;; Code:
 (server-start)
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 
-;; (setq slime-default-lisp 'clisp)
+(defvar anaconda-mode-port)
+(defvar js2-basic-offset)
+(defvar js2-strict-trailing-comma-warning)
+
 (setq anaconda-mode-port 9000)
+(setq js2-basic-offset 2)
+(setq js2-strict-trailing-comma-warning nil)
+
+(provide 'init)
+;;; init.el ends here
