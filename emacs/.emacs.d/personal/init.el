47f7ca400bc20d5e46a678d2b90c92e3059bf558
Mark Ferlatte <ferlatte@cryptio.net>
2017-02-23 21:14:18 -0800

    Make eslint work.

--- emacs/.emacs.d/personal/init.el
+++ emacs/.emacs.d/personal/init.el
@@ -13,5 +13,17 @@
 (setq js2-basic-offset 2)
 (setq js2-strict-trailing-comma-warning nil)
 
+(defun my/use-eslint-from-node-modules ()
+  "Use local eslint in node_modules instead of global install."
+  (defvar flycheck-javascript-eslint-executable)
+  (let* ((root (locate-dominating-file
+                 (or (buffer-file-name) default-directory)
+                 "node_modules"))
+          (eslint (and root
+                       (expand-file-name "node_modules/.bin/eslint" root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (provide 'init)
 ;;; init.el ends here
